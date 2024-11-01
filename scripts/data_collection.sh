64af7a96fa5290ee307868bd6adfd07799823689
Mahmoud <mahmoud.jahanshahi@gmail.com>
2024-11-01 16:51:30 -0400

    p2P

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -12,9 +12,30 @@ for i in {0..127}; do
 done
 rm data/split/blobs.{0..127}.gz
 
-# b2P
+# b2p
 zcat data/the-stack-v2-train.gz | 
 sed 's|snapshot_id.*||;s|directory_id.*repo_name||' | 
 awk -F\' '{OFS=";";print $4,$8}' | 
 sed 's|/|_|' |
 ~/lookup/splitSec.perl data/split/b2p. 128
+for i in {0..127}; do
+    zcat "data/split/b2p.$i.gz" |
+    ~/utils/sort.sh -t\; -u |
+    gzip >"data/split/b2p.$i"
+
+    zcat "data/split/b2p.$i.gz" |
+    cut -d\; -f2 |
+    ~/utils/sort.sh -t\; -u |
+    gzip >"data/split/p.$i"
+done
+
+# p2P
+dir="/nfs/home/audris/work/gz"
+
+zcat "$dir/p2PV.s" |
+~/utils/sort.sh -t\; -u |
+gzip >data/p2PV.s
+
+zcat "$dir/p2PFull.V3.s" |
+~/utils/sort.sh -t\; -u |
+gzip >data/p2PV3.s
