bf7f61ebab49a04ef09770be6850276f5e3489bd
Mahmoud <mahmoud.jahanshahi@gmail.com>
2024-11-13 10:42:13 -0500

    added license info

--- scripts/data_collection.sh
+++ scripts/data_collection.sh
@@ -3,9 +3,10 @@
 # region 1 - b2p
 
 ## full
-zcat data/the-stack-v2-train.gz | 
-sed "s|', 'snapshot_id.*||;s|.*content_id': '||;s|', .*'repo_name': '|;|;s|/|_|" |
-~/lookup/splitSec.perl data/full/b2p. 128
+zcat data/the-stack-v2-train.gz |  
+sed "s|', 'snapshot_id.*||;s|.*content_id': '||;s|'.*license_type': '|;|;s|'.*repo_name': '|;|;s|/|_|" |
+~/lookup/splitSec.perl data/full/b2lp. 128
+
 
 ## smol
 zcat data/the-stack-v2-train-smol-ids.train.gz |
@@ -14,29 +15,29 @@ perl -ane '
     ($p,@rest)=split(/;/);
     $r=join ";",@rest;
     print "$p";
-    while ($r =~ m/.content_id.: .([0-9a-f]{40})./g) { 
-        print ";".$1;
+    while ($r =~ m/.content_id.: .([0-9a-f]{40}).*?license_type.: .([^'\'']+)./g) { 
+        print ";".$1.";".$2;
     }
     print "\n";
 ' |
 awk -F\; '{
     OFS = ";" 
-    for (i=2; i<=NF; i++) {
-        print $i,$1
+    for (i=2; i<=NF; i+=2) {
+        print $i, $(i+1), $1
     }
 }' |
-~/lookup/splitSec.perl data/smol/b2p. 128
+~/lookup/splitSec.perl data/smol/b2lp. 128
 
 ## sort
 for i in {0..127}; do
     for b in {full,smol}; do
-        zcat "data/$b/b2p.$i.gz" |
+        zcat "data/$b/b2lp.$i.gz" |
         ~/utils/sort.sh -t\; -u |
-        gzip >"data/$b/b2p.$i"
+        gzip >"data/$b/b2lp.$i"
     done
 done
 for b in {full,smol}; do
-    rm data/$b/b2p.{0..127}.gz
+    rm data/$b/b2lp.{0..127}.gz
 done
 
 # endregion 1
@@ -62,29 +63,29 @@ gzip >data/p2PV3.s
 # region 3 - b2P
 for i in {0..127}; do
     for b in {full,smol}; do
-        LC_ALL=C LANG=C join -t\; -a1 -1 2 -o 1.1 1.2 2.2 \
-            <(zcat "data/$b/b2p.$i" |
-                ~/utils/sort.sh -t\; -k2,2) \
+        LC_ALL=C LANG=C join -t\; -a1 -1 3 -o 1.1 1.2 1.3 2.2 \
+            <(zcat "data/$b/b2lp.$i" |
+                ~/utils/sort.sh -t\; -k3,3) \
             <(zcat data/p2PV.s |
                 ~/utils/sort.sh -t\; -k1,1) |
         ~/utils/sort.sh -t\; -u |
-        gzip >"data/$b/b2pP.$i"
+        gzip >"data/$b/b2lpP.$i"
 
-        zcat "data/$b/b2pP.$i" |
+        zcat "data/$b/b2lpP.$i" |
         awk -F\; '{
             OFS = ";"
-            if ($3 == "") {
-                print $1,$2
+            if ($4 == "") {
+                print $1,$2,$3
             } else {
-                print $1,$3
+                print $1,$2,$4
             }
         }' |
         ~/utils/sort.sh -t\; -u |
-        gzip >"data/$b/b2P.$i"
+        gzip >"data/$b/b2lP.$i"
     done
 done
 for b in {full,smol}; do
-    rm data/$b/b2p.{0..127}
+    rm data/$b/b2lp.{0..127}
 done
 
 # endregion 3
