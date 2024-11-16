fd736d7be282d24642302623dd14daa0b50ba7b0
Mahmoud <mahmoud.jahanshahi@gmail.com>
2024-11-16 17:41:54 -0500

    replication table 2

---
+++ replication/replication.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# blob sample
+
+## full
+zcat data/bugs/b2pFull.0.s |
+cut -d\; -f1 |
+~/utils/sort.sh -u |
+gzip >data/bugs/b.full.gz
+
+zcat data/bugs/b2pobcfFull.0.s |
+cut -d\; -f1,3,4 |
+~/utils/sort.sh -u |
+gzip >data/bugs/b2obc.full.gz
+
+zcat data/bugs/b2pbcfFull.0.s | 
+cut -d\; -f1,3,4 |
+~/utils/sort.sh -u |
+gzip >data/bugs/b2nbc.full.gz
+
+
+## smol
+zcat data/bugs/bSmall.0.gz |
+~/utils/sort.sh -u |
+gzip >data/bugs/b.smol.gz
+
+zcat data/bugs/b2obcf.0.s |
+cut -d\; -f1-3 |
+~/utils/sort.sh -u |
+gzip >data/bugs/b2obc.smol.gz
+
+zcat data/bugs/b2bcf.0.s |
+cut -d\; -f1-3 |
+~/utils/sort.sh -u |
+gzip >data/bugs/b2nbc.smol.gz
+
+
+# commit sample
+
+## full
+zcat data/bugs/c2obbf.0.s |
+awk -F\; '{OFS=";"; if ($3!="") print $1,$2,$3}' |
+~/utils/sort.sh -u |
+gzip >data/bugs/c2bnb.full.gz
+
+## smol
+zcat data/bugs/c2obbfSmall.0.s |
+awk -F\; '{OFS=";"; if ($3!="") print $1,$2,$3}' |
+~/utils/sort.sh -u |
+gzip >data/bugs/c2bnb.smol.gz
+
--- replication/vulnerabilities.sh
+++ replication/vulnerabilities.sh
@@ -1,76 +1,112 @@
 #!/bin/bash
 
-# full
-zcat data/bugs/b2pFull.0.s |
-cut -d\; -f1 |
-~/utils/sort.sh -u |
-gzip >data/bugs/b.full.s
-
-zcat data/bugs/b2pobcfFull.0.s |
-cut -d\; -f1,3,4 |
-~/utils/sort.sh -u |
-gzip >data/bugs/b2obc.full.s
-
-zcat data/bugs/b2pbcfFull.0.s | 
-cut -d\; -f1,3,4 |
-~/utils/sort.sh -u |
-gzip >data/bugs/b2nbc.full.s
-
-
-# smol
-zcat data/bugs/bSmall.0.gz |
-~/utils/sort.sh -u |
-gzip >data/bugs/b.smol.s
-
-zcat data/bugs/b2obcf.0.s |
-cut -d\; -f1-3 |
-~/utils/sort.sh -u |
-gzip >data/bugs/b2obc.smol.s
-
-zcat data/bugs/b2bcf.0.s |
-cut -d\; -f1-3 |
-~/utils/sort.sh -u |
-gzip >data/bugs/b2nbc.smol.s
-
 # table 1
 for batch in {full,smol}; do
     echo "Dataset: $batch"
 
-    n1=$(zcat "data/bugs/b.$batch.s" | wc -l)
+    n1=$(zcat "b.$batch.gz" | wc -l)
     echo "Total blobs: $n1"
 
-    n2=$(zcat "data/bugs/b2obc.$batch.s" "data/bugs/b2nbc.$batch.s" |
+    n2=$(zcat "b2obc.$batch.gz" "b2nbc.$batch.gz" |
         cut -d\; -f1 | ~/utils/sort.sh -u | wc -l)
     n3=$((n1 - n2))
     percent=$(echo "scale=4; ($n3/$n1*100)" | bc | awk '{printf "%.2f", $0}')
     echo "Found blobs: $n2"
     echo "Missing blobs:  $n3 - $percent%"
 
-    zcat "data/bugs/b2obc.$batch.s" | 
+    zcat "b2obc.$batch.gz" | 
     awk -F\; '{if ($2!="") print $1}' | 
-    uniq | gzip >"data/bugs/haveOld.$batch"
+    uniq | gzip >"haveOld.$batch.gz"
     join -v2 \
-        <(zcat "data/bugs/haveOld.$batch") \
-        <(zcat "data/bugs/b2obc.$batch.s" | cut -d\; -f1 | uniq) |
-    gzip >"data/bugs/firstVer.$batch"
-    n4=$(zcat "data/bugs/haveOld.$batch" | wc -l)
+        <(zcat "haveOld.$batch.gz") \
+        <(zcat "b2obc.$batch.gz" | cut -d\; -f1 | uniq) |
+    gzip >"firstVer.$batch.gz"
+    n4=$(zcat "haveOld.$batch.gz" | wc -l)
     percent=$(echo "scale=4; ($n4/$n1*100)" | bc | awk '{printf "%.2f", $0}')
     echo "Have an old version: $n4 - $percent%"
-    n5=$(zcat "data/bugs/firstVer.$batch" | wc -l )
+    n5=$(zcat "firstVer.$batch.gz" | wc -l )
     percent=$(echo "scale=4; ($n5/$n1*100)" | bc | awk '{printf "%.2f", $0}')
     echo "First version: $n5 - $percent%"
-    n6=$(join -t\; <(zcat "data/bugs/firstVer.$batch") <(zcat "data/bugs/b2nbc.$batch.s") | 
+    n6=$(join -t\; <(zcat "firstVer.$batch.gz") <(zcat "b2nbc.$batch.gz") | 
         awk -F\; '{if ($2!="") print $1}' | uniq | wc -l )
     percent=$(echo "scale=4; ($n6/$n5*100)" | bc | awk '{printf "%.2f", $0}')
     echo "First versions with newer version: $n6 - $percent%"
 
-    zcat "data/bugs/b2nbc.$batch.s" | 
+    zcat "b2nbc.$batch.gz" | 
     awk -F\; '{if ($2!="") print}' |
-    uniq | gzip >"data/bugs/haveNew.$batch"
-    n7=$(zcat "data/bugs/haveNew.$batch" | cut -d\; -f1 | uniq | wc -l)
+    uniq | gzip >"haveNew.$batch.gz"
+    n7=$(zcat "haveNew.$batch.gz" | cut -d\; -f1 | uniq | wc -l)
     percent=$(echo "scale=4; ($n7/$n1*100)" | bc | awk '{printf "%.2f", $0}')
     echo "Blobs with new version: $n7 - $percent%"
-    n8=$(zcat "data/bugs/haveNew.$batch" | cut -d\; -f2 | ~/utils/sort.sh -u | wc -l)
+    n8=$(zcat "haveNew.$batch.gz" | cut -d\; -f2 | ~/utils/sort.sh -u | wc -l)
     echo "Found new versions: $n8"
+    echo
+done
+
+# table 2
+for batch in {full,smol}; do
+    echo "Dataset: $batch"
+
+    n1=$(zcat "data/bugs/c2bnb.$batch.gz" |
+        cut -d\; -f1 | ~/utils/sort.sh -u | wc -l)
+    n2=$(zcat "data/bugs/c2bnb.$batch.gz" |
+        cut -d\; -f2 | ~/utils/sort.sh -u | wc -l)
+    n3=$(zcat "data/bugs/c2bnb.$batch.gz" |
+        cut -d\; -f3 | ~/utils/sort.sh -u | wc -l)
+    echo "Total commit: $n1 - blob: $n2 - New version: $n3"
+
+    join -t\; \
+         <(zcat "data/bugs/c2bnb.$batch.gz") \
+         <(zcat data/bugs/c2chFull.V3.0.s | cut -d\; -f1,11) |
+    grep -iwE 'fix|fixes|fixing|bug|bugs|issue|issues|patch|patches|error|errors|resolve|resolved|resolving|correct|corrects|corrected|correcting|problem|problems|debug|debugs|debugged|debugging|cve' |
+    gzip >"data/bugs/c2bnbm.$batch.fix.gz"
+
+    zcat "data/bugs/c2bnbm.$batch.fix.gz" |
+    cut -d\; -f1-3 |
+    ~/utils/sort.sh -u |
+    gzip >"data/bugs/c2bnb.$batch.fix.gz"
+
+    n4=$(zcat "data/bugs/c2bnb.$batch.fix.gz" |
+        cut -d\; -f1 | ~/utils/sort.sh -u | wc -l)
+    percent=$(echo "scale=4; ($n4/$n1*100)" | bc | awk '{printf "%.2f", $0}')
+    echo "Fix commits: $n4 - $percent%"
+    n5=$(zcat "data/bugs/c2bnb.$batch.fix.gz" |
+        cut -d\; -f2 | ~/utils/sort.sh -u | wc -l)
+    percent=$(echo "scale=4; ($n5/$n2*100)" | bc | awk '{printf "%.2f", $0}')
+    echo "Fix blobs: $n5 - $percent%"
+    n6=$(zcat "data/bugs/c2bnb.$batch.fix.gz" |
+        cut -d\; -f3 | ~/utils/sort.sh -u | wc -l)
+    percent=$(echo "scale=4; ($n6/$n3*100)" | bc | awk '{printf "%.2f", $0}')
+    echo "Fix new versions: $n6 - $percent%"
+
+    zcat "data/bugs/c2bnbm.$batch.fix.gz" |
+    grep -iw 'cve'  | 
+    perl -ane '
+        chop();
+        ($c,$b,$nb,$a)=split(/;/,$_);
+        $a=lc($a);
+        while($a=~m|(cve\-[0-9]+\-[0-9]+)|g){
+            print "$c;$b;$nb;$1\n";
+        }'|
+    uniq | gzip > "data/bugs/c2bnba.$batch.cve.gz"
+
+    n7=$(zcat "data/bugs/c2bnba.$batch.cve.gz" |
+        cut -d\; -f1 | ~/utils/sort.sh -u | wc -l)
+    percent1=$(echo "scale=4; ($n7/$n1*100)" | bc | awk '{printf "%.2f", $0}')
+    percent2=$(echo "scale=4; ($n7/$n4*100)" | bc | awk '{printf "%.2f", $0}')
+    echo "CVE commits: $n7 - total: $percent1% - fixes: $percent2%"
+    n8=$(zcat "data/bugs/c2bnba.$batch.cve.gz" |
+        cut -d\; -f2 | ~/utils/sort.sh -u | wc -l)
+    percent1=$(echo "scale=4; ($n8/$n2*100)" | bc | awk '{printf "%.2f", $0}')
+    percent2=$(echo "scale=4; ($n8/$n5*100)" | bc | awk '{printf "%.2f", $0}')
+    echo "CVE blobs: $n8 - total: $percent1% - fixes: $percent2%"
+    n9=$(zcat "data/bugs/c2bnba.$batch.cve.gz" |
+        cut -d\; -f3 | ~/utils/sort.sh -u | wc -l)
+    percent1=$(echo "scale=4; ($n9/$n3*100)" | bc | awk '{printf "%.2f", $0}')
+    percent2=$(echo "scale=4; ($n9/$n6*100)" | bc | awk '{printf "%.2f", $0}')
+    echo "CVE new versions: $n9 - total: $percent1% - fixes: $percent2%"
+    n10=$(zcat "data/bugs/c2bnba.$batch.cve.gz" |
+        cut -d\; -f4 | ~/utils/sort.sh -u | wc -l)
+    echo "Distinct CVEs: $n10"
     echo
 done
